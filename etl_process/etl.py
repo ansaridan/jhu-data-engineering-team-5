f191c1827d7c48a256c95e5cbd704dbddd916d71
ansaridan <daniel.ansari@nytimes.com>
2024-08-02 16:50:14 -0400

    reorg repo, add etl script

--- etl_process/etl.py
+++ etl_process/etl.py
@@ -1,3 +1,14 @@
+# ENSURE THIS RUNS CORRECTLY REGARDLESS OF DIRECTORY
+import os
+import sys
+# Set the working directory to the project root
+WORKDIR = "/home/awesome"
+os.chdir(WORKDIR)
+print(f"Current working directory: {os.getcwd()}")
+# Add the project root to the Python path
+sys.path.insert(0, WORKDIR)
+
+# IMPORTS
 # datatypes
 import json
 import yaml
@@ -6,133 +17,106 @@ import psycopg2
 import psycopg2.extras
 import psycopg2.extensions as psql_ext
 from psycopg2 import sql
+# custom etl functions
+from etl_process import utils as etl
+# respective datasets
+from etl_process.data_processing import station_info as info
 # computation
 import pandas as pd
 # utilities
 from pathlib import Path
-import os
 import itertools
-from urllib.request import urlopen
 # typing
 from typing import Union
 
-def drop_recreate_schema(
-    conn: psql_ext.connection,
-    schema: str
-) -> None:
-    # drop all tables
-    try:
-        with conn.cursor() as cur:
-            # Fetch all table names in the schema
-            cur.execute(sql.SQL("""
-                SELECT table_name
-                FROM information_schema.tables
-                WHERE table_schema = %s
-                AND table_type = 'BASE TABLE';
-            """), [schema])
-            
-            tables = cur.fetchall()
-            
-            if tables:
-                # Drop each table
-                for table in tables:
-                    table_name = table[0]
-                    cur.execute(sql.SQL("DROP TABLE IF EXISTS {}.{} CASCADE").format(
-                        sql.Identifier(schema),
-                        sql.Identifier(table_name)
-                    ))
-                    print(f"Table '{table_name}' dropped.")
-            
-            # Commit the changes
-            conn.commit()
-            
-    except Exception as e:
-        print(f"Error: {e}")
-        conn.rollback()
+# set up directories
+WORKDIR_PATH = Path.cwd()
+DATA_PATH = WORKDIR_PATH / 'etl_process' / 'processed_data'
+SCHEMAS_PATH = WORKDIR_PATH / 'etl_process' / 'schemas'
 
-    # drop recreate schema
-    try:
-        with conn.cursor() as cur:
-            # drop recreate
-            print(f"All tables in {schema} dropped successfully.")
-            cur.execute(f'DROP SCHEMA IF EXISTS {schema}')
-            print(f"Dropped Schema {schema}.")
-            cur.execute(f'CREATE SCHEMA IF NOT EXISTS {schema}')
-            print(f"Created Schema {schema}.")
-            # Commit the changes
-            conn.commit()
-    except Exception as e:
-        print(f"Error: {e}")
-        conn.rollback()
+PROJECT_SCHEMA = 'citibike_project'
 
-def read_yaml_to_dict(yaml_file: Union[str, Path]) -> dict:
-    """Reads a YML file to a dictionary"""
-    with open(yaml_file, 'r') as file:
-        data = yaml.safe_load(file)
-    return data
-
-def generate_create_table_statement(schema:str, table_name:str, 
- schema_data:dict) -> str:
-    columns = schema_data['columns']
+def main():
+    # PSQL db connection using psycopg2
+    with psycopg2.connect(
+        dbname='new_db', 
+        user='awesome_user', 
+        password='awesome_password', 
+        host='postgres', 
+        port='5432'
+    ) as conn:
     
-    column_definitions = []
-    for column in columns:
-        column_definitions.append(f"{column['name']} {column['type']}")
+        # [1] SET UP THE DATABASE SCHEMA
+        etl.drop_recreate_schema(conn, PROJECT_SCHEMA)
     
-    columns_str = ",\n  ".join(column_definitions)
-    create_table_statement = f"CREATE TABLE {schema}.{table_name} (\n  {columns_str}\n);"
+        # [2] SET UP THE TABLE SCHEMAS
+        schema_files = [item for item in SCHEMAS_PATH.iterdir() if item.is_file()]
+        tables_schemas = list(
+            itertools.chain(*[etl.read_yaml_to_dict(schema_file)["tables"] for schema_file in schema_files])
+        )
+        tables_schemas = {k: v for d in tables_schemas for k, v in d.items()}
+        
+        for table_name, table_schema in tables_schemas.items():
+            etl.drop_recreate_table(
+                db_schema=PROJECT_SCHEMA,
+                table_name=table_name,
+                table_schema=table_schema,
+                conn=conn,
+            )
     
-    return create_table_statement
+        # [3] UPLOAD EACH DATASET FROM CSVS
+        #        3a: Ride Data
+        traffic_data = [item for item in (DATA_PATH / "rides" / "station_traffic").iterdir() if item.is_file()]
+        
+        for file in traffic_data:
+            df = pd.read_csv(file)
+            etl.upload_dataframe(
+                conn=conn,
+                dataframe=df,
+                db_schema=PROJECT_SCHEMA,
+                table_name="citibike_station_history",
+                table_schema=tables_schemas["citibike_station_history"]
+            )
+    
+        #         3b: Weather Data
+        for file in [
+            "weather_general",
+            "weather_precip",
+        ]:
+            df = pd.read_csv(DATA_PATH / "weather" / f"{file}.csv")
+            etl.upload_dataframe(
+                conn=conn,
+                dataframe=df,
+                db_schema=PROJECT_SCHEMA,
+                table_name=file,
+                table_schema=tables_schemas[file]
+            )
+    
+    
+        #         3c: IRS Data
+        for file in [
+            "irs_codes",
+            "nyc_irs",
+        ]:
+            df = pd.read_csv(DATA_PATH / "irs" / f"{file}.csv")
+            etl.upload_dataframe(
+                conn=conn,
+                dataframe=df,
+                db_schema=PROJECT_SCHEMA,
+                table_name=file,
+                table_schema=tables_schemas[file]
+            )
+            
+        #         3d: Station Info
+        df_station_info = info.get_station_info_data()
+        
+        etl.upload_dataframe(
+            conn=conn,
+            dataframe=df_station_info,
+            db_schema=PROJECT_SCHEMA,
+            table_name='station_info',
+            table_schema=tables_schemas["station_info"]
+        )
 
-def drop_recreate_table(
-    *,
-    db_schema: str,
-    table_name: str,
-    table_schema: dict,
-    conn: psql_ext.connection
-) -> None:
-    try:
-        with conn.cursor() as cursor:
-            print(f'Dropping {db_schema}.{table_name}')
-            drop_statement = f'DROP TABLE IF EXISTS {db_schema}.{table_name}'
-            cursor.execute(drop_statement)
-            conn.commit()
-    except Exception as e:
-        print(f"Error: {e}")
-        conn.rollback()
-
-    try:
-        with conn.cursor() as cursor:
-            print(f'Creating {db_schema}.{table_name}')
-            create_statement = generate_create_table_statement(db_schema, table_name, table_schema)
-            cursor.execute(create_statement)
-            conn.commit()
-    except Exception as e:
-        print(f"Error: {e}")
-        conn.rollback()
-
-def upload_dataframe(
-    *,
-    conn: psql_ext.connection,
-    dataframe: pd.DataFrame,
-    db_schema: str,
-    table_name: str,
-    table_schema: dict,
-) -> None:
-    columns = [col["name"] for col in table_schema["columns"]]
-    data_dict = dataframe[columns].to_dict(orient='records')
-    query = 'INSERT INTO {}.{} ({}) VALUES %s'.format(
-        db_schema,
-        table_name,
-        ','.join(columns)
-    )
-    values = [[value for value in data.values()] for data in data_dict]
-    try:
-        with conn.cursor() as cursor:
-            psycopg2.extras.execute_values(cursor, query, values)
-            conn.commit()
-            print(f"Uploaded {len(dataframe)} records to {db_schema}.{table_name}")
-    except Exception as e:
-        print(f"Error uploading to {db_schema}.{table_name}:\n")
-        print(f"Error: {e}")
-        conn.rollback()
+if __name__ == "__main__":
+    main()
